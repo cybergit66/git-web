df3d97aef01b1d67294da59d70f1595b3ec8b7bd
Terence <terence.highsmith@gmail.com>
2016-09-29 13:25:18 -0500

    added new css and gulp tasks for postcss and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+var watch = require('gulp-watch'),
+    postcss = require('gulp-postcss')
+    autoprefixer = require('autoprefixer')
+    cssvars = require('postcss-simple-vars')
+    nested = require('postcss-nested');
 
 gulp.task('default', function(){
     console.log("hello world gulper");
@@ -7,6 +11,12 @@ gulp.task('default', function(){
 
 gulp.task('html', function(){
     console.log("Imagine something useful being done here");
+});
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/css/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/css'));
 });
 
 gulp.task('css', function(){
@@ -17,6 +27,10 @@ gulp.task('watch', function(){
     watch('./app/index.html', function(){
         gulp.start('html');
     });
+    
+    watch('./app/assets/css/**/*.css', function(){
+        gulp.start('styles');
+    })
 });
 
 watch('./app/assets/css/*.css', function(){
